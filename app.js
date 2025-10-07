bc357cceec937418009df3fbc05946a3a0a8af02
SabdarAnsari05 <sabdaransari0783@gmail.com>
2025-10-07 12:56:49 +0530

    creating testListing api

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const ejs = require('ejs');
+const Listing = require("./models/listing.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -20,6 +21,20 @@ async function main() {
 
 app.get('/', (req, res) => {
     res.send("I am root");
+});
+
+app.get("/testListing", async (req, res) => {
+    const sampleListing = new Listing({
+        title: "My new villa",
+        description: "Best villa in town",
+        price: 1200,
+        location: "Calangute, Goa",
+        country: "India",
+    });
+
+    await sampleListing.save();
+    console.log("sample was saved");
+    res.send("successfull testing");
 });
 
 app.listen(8080, () => {
