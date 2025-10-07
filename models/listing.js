16420a6ccadb73f40136edf927e45de1a1d42451
SabdarAnsari05 <sabdaransari0783@gmail.com>
2025-10-07 12:52:52 +0530

    added listing.js

---
+++ models/listing.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema ({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: String,
+    image: {
+    type: String,
+    default: "https://unsplash.com/photos/white-concrete-building-g39p1kDjvSY",
+    set: (v) =>
+        v === "" 
+        ? "https://unsplash.com/photos/white-concrete-building-g39p1kDjvSY" 
+        : v,
+    },
+    price: Number,
+    location: String,
+    country: String,
+});
+
+const Listing = mongoose.model("Listing", listingSchema);
+module.exports = Listing;
