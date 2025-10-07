da79286af04671a614735d34277ecd20bba42a5b
SabdarAnsari05 <sabdaransari0783@gmail.com>
2025-10-07 12:46:39 +0530

    connected to mongodb

--- app.js
+++ app.js
@@ -3,6 +3,21 @@ const app = express();
 const mongoose = require('mongoose');
 const ejs = require('ejs');
 
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+
+main()
+    .then(() => {
+        console.log("Connected to MongoDB");
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+
+async function main() {
+    await mongoose.connect(MONGO_URL);
+}
+
+
 app.get('/', (req, res) => {
     res.send("I am root");
 });
