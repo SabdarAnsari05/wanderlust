ddc13c49e182979531cdb630a2a651988149f2b8
SabdarAnsari05 <sabdaransari0783@gmail.com>
2025-10-10 00:18:17 +0530

    Added index route

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require('mongoose');
 const ejs = require('ejs');
 const Listing = require("./models/listing.js");
+const path = require('path');
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -18,25 +19,19 @@ async function main() {
     await mongoose.connect(MONGO_URL);
 }
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
-app.get('/', (req, res) => {
+app.get('/', (req, res) =>   {
     res.send("I am root");
 });
 
-app.get("/testListing", async (req, res) => {
-    const sampleListing = new Listing({
-        title: "My new villa",
-        description: "Best villa in town",
-        price: 1200,
-        location: "Calangute, Goa",
-        country: "India",
-    });
-
-    await sampleListing.save();
-    console.log("sample was saved");
-    res.send("successfull testing");
+// Index Route
+app.get('/listings', async (req, res) => {
+    const allListings = await Listing.find({});
+    res.render("listings/index.ejs", {allListings});
 });
-
+git
 app.listen(8080, () => {
     console.log('Server is running on http://localhost:8080');
 });
