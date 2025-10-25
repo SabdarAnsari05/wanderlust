405dc47d688a3db0e56f51d084946fb7cb4a7432
SabdarAnsari05 <sabdaransari0783@gmail.com>
2025-10-25 23:18:54 +0530

    feat: add custom error handling middleware and try-catch in create route

--- app.js
+++ app.js
@@ -1,85 +1,93 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
-const ejs = require('ejs');
+const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
-const path = require('path');
-const methodOverride = require('method-override');
-const ejsMate = require('ejs-mate');
+const path = require("path");
+const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
 main()
-    .then(() => {
-        console.log("Connected to MongoDB");
-    })
-    .catch((err) => {
-        console.log(err);
-    });
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 async function main() {
-    await mongoose.connect(MONGO_URL);
+  await mongoose.connect(MONGO_URL);
 }
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-app.use(express.urlencoded({extended: true}));
-app.use(methodOverride('_method'));
-app.engine('ejs', ejsMate);
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
 
 // Root Route
-app.get('/', (req, res) =>   {
-    res.send("I am root");
+app.get("/", (req, res) => {
+  res.send("I am root");
 });
 
 // Index Route
-app.get('/listings', async (req, res) => {
-    const allListings = await Listing.find({});
-    res.render("listings/index.ejs", {allListings});
+app.get("/listings", async (req, res) => {
+  const allListings = await Listing.find({});
+  res.render("listings/index.ejs", { allListings });
 });
 
 // New Route
-app.get('/listings/new', async (req, res) => {
-    res.render("listings/new.ejs");
+app.get("/listings/new", (req, res) => {
+  res.render("listings/new.ejs");
 });
 
 // Show Route
-app.get('/listings/:id', async (req, res) => {
-    const {id} = req.params;
-    const listing = await Listing.findById(id);
-    res.render("listings/show.ejs", {listing});
+app.get("/listings/:id", async (req, res) => {
+  const { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/show.ejs", { listing });
 });
 
 // Create Route
-app.post('/listings', async (req, res) => {
+app.post("/listings", async (req, res, next) => {
+  try {
     const newListing = new Listing(req.body.listing);
-    await newListing.save(); 
+    await newListing.save();
     res.redirect("/listings");
+  } catch (err) {
+    next(err);
+  }
 });
 
 // Edit Route
-app.get('/listings/:id/edit', async (req, res) => {
-    const {id} = req.params;
-    const listing = await Listing.findById(id);
-    res.render("listings/edit.ejs", {listing});
+app.get("/listings/:id/edit", async (req, res) => {
+  const { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/edit.ejs", { listing });
 });
 
 // Update Route
-app.put('/listings/:id', async (req, res) => {
-    const {id} = req.params;
-    const listing = await Listing.findByIdAndUpdate(id, {...req.body.listing});
-    res.redirect(`/listings/${id}`);
+app.put("/listings/:id", async (req, res) => {
+  const { id } = req.params;
+  const listing = await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+  res.redirect(`/listings/${id}`);
 });
 
 // Delete Route
-app.delete('/listings/:id', async (req, res) => {
-    const {id} = req.params;
-    let deletedListing = await Listing.findByIdAndDelete(id);
-    console.log(deletedListing);
-    res.redirect('/listings');
+app.delete("/listings/:id", async (req, res) => {
+  const { id } = req.params;
+  let deletedListing = await Listing.findByIdAndDelete(id);
+  console.log(deletedListing);
+  res.redirect("/listings");
+});
+
+// middleware to handle async error
+app.use((err, req, res, next) => {
+  res.send("something went wrong!");
 });
 
 app.listen(8080, () => {
-    console.log('Server is running on http://localhost:8080');
+  console.log("Server is running on http://localhost:8080");
 });
