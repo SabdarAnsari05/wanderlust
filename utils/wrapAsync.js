00d8012e0756682a4394073bb2af927f5b94ee6c
SabdarAnsari05 <sabdaransari0783@gmail.com>
2025-10-26 10:29:47 +0530

    Added wrapAsync utility function to handle async errors

--- utils/wrapAsync.js
+++ utils/wrapAsync.js
@@ -1,11 +1,5 @@
-// function wrapAsync(fn) {
-//     return function(req, res, next) {
-//         fn(req, res, next). catch(next);
-//     }
-// }
-
 module.exports = (fn) => {
     return (req, res, next) => {
-        fn(req, res, next). catch(next);
+        fn(req, res, next).catch(next);
     };
-};
+}
