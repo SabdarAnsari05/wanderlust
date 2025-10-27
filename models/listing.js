d529917345c6dcc99388a9577ee0661d363d8aaf
SabdarAnsari05 <sabdaransari0783@gmail.com>
2025-10-27 12:56:03 +0530

    created Review model and added review field to Listing schema

--- models/listing.js
+++ models/listing.js
@@ -1,27 +1,31 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const listingSchema = new Schema ({
-    title: {
-        type: String,
-        required: true,
-    },
-    description: String,
-    image: {
+const listingSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: String,
+  image: {
     type: String,
     default: "https://unsplash.com/photos/white-concrete-building-g39p1kDjvSY",
     set: (v) =>
-        v === "" 
-        ? "https://unsplash.com/photos/white-concrete-building-g39p1kDjvSY" 
+      v === ""
+        ? "https://unsplash.com/photos/white-concrete-building-g39p1kDjvSY"
         : v,
+  },
+  price: {
+    type: Number,
+  },
+  location: String,
+  country: String,
+  reviews: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Review",
     },
-    price: {
-        type: Number,
-        required: true,
-        default: 0,
-    },
-    location: String,
-    country: String,
+  ],
 });
 
 const Listing = mongoose.model("Listing", listingSchema);
