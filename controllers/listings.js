131cd51d8e5b96a87a3902eba80e55010217a96c
SabdarAnsari05 <sabdaransari0783@gmail.com>
2025-11-11 23:17:43 +0530

    Handle Cloudinary image upload and save URL + filename

--- controllers/listings.js
+++ controllers/listings.js
@@ -29,10 +29,14 @@ module.exports.showListing = async (req, res) => {
 };
 
 module.exports.createListing = async (req, res, next) => {
+  let url = req.file.path;
+  let filename = req.file.filename;
+
   const newListing = new Listing(req.body.listing);
-  req.flash("success", "New Listing Created Successfully!");
   newListing.owner = req.user._id;
+  newListing.image = {url, filename};
   await newListing.save();
+  req.flash("success", "New Listing Created Successfully!");
   res.redirect("/listings");
 };
 
